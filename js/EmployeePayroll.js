fe05330aa6d78541e41a61a97db3d9d3ad250fdc
dev01997D <devnandan01997@gmail.com>
2020-11-18 17:59:37 +0530

    Modified: EmployeePayroll class was modified and various new attributes were added

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,65 @@
+{
+    class EmployeePayrollData {
+
+        //Getters and setters
+        get id() { return this._id; }
+        set id(id) {
+            this._id = id;
+        }
+
+        get name() { return this._name; }
+        set name(name) {
+            this._name = name;
+        }
+
+        get gender() { return this._gender; }
+        set gender(gender) {
+            this._gender = gender;
+        }
+
+        get profilePic() { return this._profilePic; }
+        set profilePic(profilePic) {
+            this._profilePic = profilePic;
+        }
+
+        get department() { return this._department; }
+        set department(department) {
+            this._department = department;
+        }
+
+        get salary() { return this._salary; }
+        set salary(salary) {
+            this._salary = salary;
+        }
+
+        get notes() { return this._notes; }
+        set notes(notes) {
+            this._notes = notes;
+        }
+
+        get startDate() { return this._startDate; }
+        set startDate(startDate) {
+            this._startDate = startDate;
+        }
+
+        //Method
+        toString() {
+            //Options is used to format the output of date into user requirement
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            const empDate = this.startDate === undefined ? "undefined" :
+                this.startDate.toLocaleDateString("en-US", options);
+            return "Id : " + this.id + ", Name : " + this.name + ", Profile pic : " + this.profilePic + ", gender : " + this.gender + ", Department : " + this.department + ", Salary : " + this.salary
+                + ", Notes : " + this.notes + ", Start Date : " + empDate;
+        }
+    }
+    empPayrollData = new EmployeePayrollData(1, 'Dev', 'Male', 'undefined', 'HR', 500000.00, 'This is new notes', new Date(2018, 11, 24));
+    empPayrollData.id = 1;
+    empPayrollData.name = 'Dev';
+    empPayrollData.gender = 'Male';
+    empPayrollData.profilePic = 'E:\employeePayrollJSONServer\assets\Ellipse -4.png';
+    empPayrollData.department = 'HR';
+    empPayrollData.salary = 500000.00;
+    empPayrollData.notes = 'India is my country';
+    empPayrollData.startDate = new Date();
+    console.log(empPayrollData.toString());
+}
